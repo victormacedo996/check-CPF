e7d662307956b3a734c90d988643f7ce83102ec3
victor macedo <victormacedo996@gmail.com>
2021-02-25 09:12:52 -0300

    Files to create PyPI package

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
-from distutils.core import setup
+
+from setuptools import setup
+
 setup(
-  name = 'check-CPF',         # How you named your package folder (MyLib)
-  packages = ['check-CPF'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Package to verify if the inputed CPF is valid or not. It returns a interger eleven character number',   # Give a short description about your library
-  author = 'Victor Macedo',                   # Type in your name
-  author_email = 'victormacedo996@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/victormacedo996/check-CPF',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['CPF', 'validate', 'verify', 'Cadastro de pessoa física'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-      ],
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+    name = 'check-CPF',
+    version = 'v0.1.0',
+    author = 'Victor Macedo',
+    author_email = 'victormacedo996@gmail.com',
+    packages = ['check-CPF'],
+    description = 'Tool to verify if the inputed CPF is a valid CPF or not. It returns a eleven digit interger number',
+    long_description = 'It returns a eleven digit interger number. ',
+    url = 'https://github.com/victormacedo996/check-CPF',
+    project_urls = {
+        'Código fonte': 'https://github.com/victormacedo996/check-CPF',
+        'Download': 'https://github.com/yanorestes/aluratemp/archive/1.0.0.zip'
+    },
+    license = 'MIT',
+    keywords = 'CPF' 'validate' 'validar' 'cadastro de pessoa física',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English (US)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Internationalization'
+    ]
 )
